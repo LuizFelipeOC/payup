ba1269ae5825f11d90a899e0a55bd34b42a43bef
_costa <luizfelipeeoliveiraac@gmail.com>
2026-03-23 19:15:23 -0400

    feat: add Vector SVG icon to AppIcons

--- lib/core/shared/images/app_icons.dart
+++ lib/core/shared/images/app_icons.dart
@@ -12,4 +12,5 @@ class AppIcons {
   static const String shield = 'assets/icons/shield.svg';
   static const String trash = 'assets/icons/trash 1.svg';
   static const String logo = 'assets/icons/logo.svg';
+  static const String vector = 'assets/icons/Vector.svg';
 }
