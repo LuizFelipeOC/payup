f935bfb7879e088e2065e205227510806e89176b
_costa <luizfelipeeoliveiraac@gmail.com>
2026-03-23 19:03:36 -0400

    Add new SVG icons and update asset references

---
+++ lib/core/shared/images/app_icons.dart
@@ -0,0 +1,14 @@
+class AppIcons {
+  static const String alertCircle = 'assets/icons/alert-circle.svg';
+  static const String buildings = 'assets/icons/buildings.svg';
+  static const String calendarDollar = 'assets/icons/calendar dollar.svg';
+  static const String calendar = 'assets/icons/calendar.svg';
+  static const String check = 'assets/icons/check.svg';
+  static const String coins = 'assets/icons/coins.svg';
+  static const String down = 'assets/icons/down.svg';
+  static const String faceId = 'assets/icons/face id.svg';
+  static const String filter = 'assets/icons/filter.svg';
+  static const String search = 'assets/icons/search.svg';
+  static const String shield = 'assets/icons/shield.svg';
+  static const String trash = 'assets/icons/trash 1.svg';
+}
