595154ed0e0127deffb98c1a14aea0d1dd358f07
_costa <luizfelipeeoliveiraac@gmail.com>
2026-03-24 20:33:38 -0400

    feat: integrate HomeWeeklyFilter into HomePage layout

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:payup/screens/home/widgets/home_weekly_filter.dart';
 import 'package:payup/screens/home/widgets/user_app_bar.dart';
 
 class HomePage extends StatefulWidget {
@@ -11,6 +12,14 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(appBar: UserAppBar());
+    return Scaffold(
+      appBar: UserAppBar(),
+      body: CustomScrollView(
+        slivers: [
+          SliverToBoxAdapter(child: SizedBox(height: 55)),
+          HomeWeeklyFilter(),
+        ],
+      ),
+    );
   }
 }
