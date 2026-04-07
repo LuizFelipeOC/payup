0800a9511ff57b39bf50489533555efc85a8e3e0
_costa <luizfelipeeoliveiraac@gmail.com>
2026-04-06 20:15:25 -0400

    feat: add localization support for Portuguese in PayupWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:payup/payup_widget.dart';
+import 'package:intl/date_symbol_data_local.dart';
 
 void main() {
+  initializeDateFormatting('pt_BR', null);
+
   runApp(PayupWidget());
 }
--- lib/payup_widget.dart
+++ lib/payup_widget.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:payup/core/themes/app_theme.dart';
 import 'package:payup/screens/splash/splash_screen.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 class PayupWidget extends StatelessWidget {
   const PayupWidget({super.key});
@@ -11,6 +12,13 @@ class PayupWidget extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: AppTheme.lightTheme,
       home: SplashScreen(),
+      locale: const Locale('pt', 'BR'),
+      supportedLocales: const [Locale('pt', 'BR')],
+      localizationsDelegates: const [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate,
+      ],
     );
   }
 }
