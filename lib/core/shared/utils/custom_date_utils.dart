1e20a84c5d74232a91aee70c7cfa1e8ebca9dd5d
_costa <luizfelipeeoliveiraac@gmail.com>
2026-04-06 20:15:31 -0400

    feat: implement CustomDateUtils for retrieving current week days in Portuguese

---
+++ lib/core/shared/utils/custom_date_utils.dart
@@ -0,0 +1,20 @@
+import 'package:intl/intl.dart';
+import 'package:payup/core/models/week_day_model.dart';
+
+class CustomDateUtils {
+  List<WeekDayModel> getCurrentWeekDays() {
+    final now = DateTime.now();
+    final firstDay = now.subtract(Duration(days: now.weekday - 1));
+    final formatter = DateFormat.E('pt_BR');
+
+    return List.generate(7, (index) {
+      final date = firstDay.add(Duration(days: index));
+
+      return WeekDayModel(
+        index: date.weekday,
+        label: formatter.format(date).toLowerCase(),
+        date: date,
+      );
+    });
+  }
+}
