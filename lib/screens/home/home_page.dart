2bbdce24b81456ecf2282802def50eb8463e5e0e
_costa <luizfelipeeoliveiraac@gmail.com>
2026-04-11 14:51:03 -0400

    feat: enhance HomePage with payment list handling and loading state display

--- lib/screens/home/home_page.dart
+++ lib/screens/home/home_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:payup/core/shared/widgets/buttons/button.dart';
+import 'package:payup/core/shared/widgets/card_payments/card_payments.dart';
+import 'package:payup/screens/home/widgets/home_weekly_filter/home_weekly_controller.dart';
 import 'package:payup/screens/home/widgets/home_weekly_filter/home_weekly_filter.dart';
 import 'package:payup/screens/home/widgets/user_app_bar.dart';
 
@@ -10,6 +13,16 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  final listPaymentsToReceive = [];
+
+  final HomeWeeklyController controller = HomeWeeklyController();
+
+  @override
+  void initState() {
+    controller.initialize();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +30,45 @@ class _HomePageState extends State<HomePage> {
       body: CustomScrollView(
         slivers: [
           SliverToBoxAdapter(child: SizedBox(height: 55)),
-          HomeWeeklyFilter(callbackFunction: (value) {}),
+          HomeWeeklyFilter(
+            callbackFunction: (value) {
+              controller.setSelectedWeekDay(value);
+              controller.isToday(value);
+            },
+          ),
+
+          if (listPaymentsToReceive.isEmpty) ...[
+            SliverToBoxAdapter(child: SizedBox(height: 20)),
+            SliverToBoxAdapter(
+              child: Padding(
+                padding: const EdgeInsets.only(left: 10, right: 10),
+                child: ValueListenableBuilder(
+                  valueListenable: controller,
+                  builder: (_, _, _) {
+                    if (controller.isLoading) return const SizedBox();
+
+                    final isToday = controller.isTodayValue.value;
+
+                    return Text(
+                      isToday ? 'Hoje' : controller.value?.label ?? '',
+                      style: Theme.of(context).textTheme.titleMedium,
+                    );
+                  },
+                ),
+              ),
+            ),
+            SliverToBoxAdapter(child: SizedBox(height: 20)),
+
+            SliverToBoxAdapter(child: CardPayments(type: CardPaymentType.empty)),
+          ],
+
+          SliverToBoxAdapter(child: SizedBox(height: 48)),
+          SliverToBoxAdapter(
+            child: Padding(
+              padding: const EdgeInsets.only(right: 10, left: 10),
+              child: Button(label: 'Adicionar Cliete', onPressed: () {}),
+            ),
+          ),
         ],
       ),
     );
