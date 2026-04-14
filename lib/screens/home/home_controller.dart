66d3aa81844c35123a85a2846bdc36833044cded
_costa <luizfelipeeoliveiraac@gmail.com>
2026-04-14 19:42:56 -0400

    feat: add HomeController to manage client data

---
+++ lib/screens/home/home_controller.dart
@@ -0,0 +1,9 @@
+import 'package:payup/core/models/client_model.dart';
+
+class HomeController {
+  List<ClientModel> clients = [
+    ClientModel(name: 'Aurora Tech Soluções Digitais'),
+    ClientModel(name: 'Veltrix Labs'),
+    ClientModel(name: 'Orbitum Tech'),
+  ];
+}
